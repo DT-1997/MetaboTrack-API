b76c4884c2c58de325533063608fd780b029c425
DT-1997 <hx3026678597@163.com>
2026-04-06 21:26:20 +0800

    initialize Spring Boot project structure

---
+++ src/main/java/com/metabotrackapi/MetaboTrackApiApplication.java
@@ -0,0 +1,13 @@
+package com.metabotrackapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaboTrackApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetaboTrackApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/metabotrackapi/MetaboTrackApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metabotrackapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetaboTrackApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
